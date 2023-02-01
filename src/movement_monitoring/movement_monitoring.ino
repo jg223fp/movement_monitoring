9e9e2c65d735ee2fc29ff5120083362254cd0aa1
johan_laptop <jg223fp@student.lnu.se>
2023-02-01 13:13:13 +0100

    Added tempsensor to same core as led for testing

--- src/movement_monitoring/movement_monitoring.ino
+++ src/movement_monitoring/movement_monitoring.ino
@@ -1,6 +1,12 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
+
+// Global variables
+Adafruit_AMG88xx amg;
+float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
 // Tasks
 void blink(void *param) {
@@ -14,10 +20,49 @@ void blink(void *param) {
   }
 }
 
+void amgInit() {
+    Serial.println("Initiating AMG8833");
+
+    bool status;
+    
+    status = amg.begin();
+    if (!status) {
+        Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+        while (1);
+    }
+    delay(100); // let sensor boot up
+}
+
+void amgRead(void *param) {
+  (void) param;
+  //read all the pixels
+    amg.readPixels(pixels);
+
+    Serial.print("[");
+    for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
+      Serial.print(pixels[i-1]);
+      Serial.print(", ");
+      if( i%8 == 0 ) Serial.println();
+    }
+    Serial.println("]");
+    Serial.println();
+
+    //delay a second
+    delay(1000);
+}
+
+
+
+
+
+
+
 // Running on core0
 void setup() {
   Serial.begin(115200);
+  amgInit();
   xTaskCreate(blink, "BLINK", 128, nullptr, 1, nullptr);
+  xTaskCreate(amgRead, "TempRead", 128, nullptr, 2, nullptr);
   delay(5000);
 }
 
