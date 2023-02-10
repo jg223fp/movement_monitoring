256fcc5dffab4e4405201b374b7ce3553c4d8990
johan_laptop <jg223fp@student.lnu.se>
2023-02-10 13:31:18 +0100

    Added code for 16 pixels option and structured code

--- src/temp_left_right/temp_left_right.ino
+++ src/temp_left_right/temp_left_right.ino
@@ -11,13 +11,12 @@ float pixels[64];
 
 float a = 0;   // left block
 float b = 0;   // right block
-float hysteres = 0.4;  //higher is less sensitivity, lower more noise   0.4 is best so far
 bool leftFlag = false;
 bool rightFlag = false;
-int loopsWithFlag = 0;   // Number of spins with a flag set.
-int flagLoopLimit = 10; // How many spins the loop can go with a flag set, waiting for a human to enter the other block.
+int loopsWithFlag = 0;   // Number of spins with a flag set. Counting variable. Can not be adjusted.
+float hysteres = 0.5;  // Higher gives less sensitivity, lower more noise   0.4 is best so far
+int flagLoopLimit = 10; // How many spins the loop can go with a flag set, waiting for a human to enter the other block. 10 with all 64 pixels
 int inRoom = 0;
-
 
 void setup() {
     Serial.begin(115200);
@@ -37,38 +36,32 @@ void setup() {
 
     delay(100); // let sensor boot up
 
-    
+    // Set led outputs
+    pinMode (6,OUTPUT); // RED led
+    pinMode (8,OUTPUT); // Green led
 
-    pinMode (6,OUTPUT);//left light
-    pinMode (8,OUTPUT);//right light
-
-    digitalWrite(6,HIGH);//turn leds on
+    //turn leds on
+    digitalWrite(6,HIGH);
     digitalWrite(8,HIGH);
 
     delay(3000);
 
+    //Turn leds off
     digitalWrite(6,LOW);
     digitalWrite(8,LOW);
 }
 
 
 void loop() { 
-    //read all the pixels
+    // Read all the pixels
     amg.readPixels(pixels);
 
-
-
+    // Write all LEDS low
     digitalWrite (6,LOW);
     digitalWrite (8,LOW);
 
-    //delay(100);
-
     
-//remove comments fron this section if you want to see all values
-//obtained in the arrary--suggest that you add 15 second delay
-//at end of program so that things won't scroll too quickly
-    
-/*
+/*   // Code for printing all pixels
     Serial.print("[");
     //for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
     for(int i=1; i<=64; i++) {
@@ -84,7 +77,19 @@ void loop() {
     //delay(15000);
 
 */
+     // 16 pixels: the two rows in the middle on each side
+    for (int i=2; i<=63; i=i+8) {
+      a = a + pixels[i];
+      a = a + pixels[i+1];
+      b = b + pixels[i+2];
+      b = b + pixels[i+3];
+    }
 
+    //obtain average value for each block
+      a = (a/16);
+      b = (b/16);
+
+/* // 64 pixels. 32 in each block
 // divide pixels into two blocks. 
     for (int i=0; i<=63; i=i+8) {
       for (int j = 0; j<4; j++) {
@@ -106,54 +111,47 @@ void loop() {
 */
 
 // check how many spins a flag has been raised. If nothing happens, reset the flags
-if (leftFlag || rightFlag) {
-  loopsWithFlag += 1;
+    if (leftFlag || rightFlag) {
+      loopsWithFlag += 1;
 
-  if (loopsWithFlag > flagLoopLimit) {
-  leftFlag = false;
-  rightFlag = false;
-  loopsWithFlag = 0;
-  }
-}
-
-
-
-
-
-
-
-
-// check left block
-if (a>b) {
-  if(rightFlag) {
-      inRoom = inRoom - 1;
+      if (loopsWithFlag > flagLoopLimit) {
+      leftFlag = false;
       rightFlag = false;
-    } else {
-      if (a-b > hysteres) {
-      digitalWrite (6,HIGH);
-      leftFlag = true;
+      loopsWithFlag = 0;
       }
     }
-}
 
-// check right block
-if ((b>a)) {
-  if(leftFlag) {
-    inRoom = inRoom + 1;
-    leftFlag = false;
-  } else {
-    if (b-a > hysteres) {
-    digitalWrite (8,HIGH);
-    rightFlag = true;
+
+    // check left block
+    if (a>b) {
+      if(rightFlag) {
+          inRoom = inRoom - 1;
+          rightFlag = false;
+        } else {
+          if (a-b > hysteres) {
+          digitalWrite (6,HIGH);
+          leftFlag = true;
+          }
+        }
     }
-  }
-}
+
+    // check right block
+    if ((b>a)) {
+      if(leftFlag) {
+        inRoom = inRoom + 1;
+        leftFlag = false;
+      } else {
+        if (b-a > hysteres) {
+        digitalWrite (8,HIGH);
+        rightFlag = true;
+        }
+      }
+    }
 
 
-Serial.println(inRoom);
-//delay(100);
-a=0;
-b=0;
+    Serial.println(inRoom);
+    a=0;
+    b=0;
 }
          
 
