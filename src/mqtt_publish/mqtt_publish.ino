52ea94e1bb4d31587b60915f3a08e8a294ceb64c
jg223fp <jg223fp@student.lnu.se>
2023-02-21 09:30:11 +0100

    Wrote mqtt publisher example code

---
+++ src/mqtt_publish/mqtt_publish.ino
@@ -0,0 +1,89 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "3807444";
+const char* password = "berlin2022";
+
+// MQTT Broker IP address
+const char* mqtt_server = "io.adafruit.com";
+const int mqttPort = 1883;
+#define IO_USERNAME  "jg223fp"
+#define IO_KEY       "aio_pHcJ80PKSkjPHgfLR486iaNrdY6m"
+String clientId = "movement_sensor_box";
+const char* mqttTopic = "jg223fp/feeds/Dayli_flow";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+
+float temperature = 0;
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqttPort);  // move to reconnect function
+  //client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), IO_USERNAME, IO_KEY)) {
+            Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Temperature in Celsius
+    temperature = 12;   
+
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    client.publish(mqttTopic, tempString);
+  }
+}
