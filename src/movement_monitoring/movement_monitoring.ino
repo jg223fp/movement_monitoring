b10c121e6cb48c2aac71ed6387067f46c9a5f0ed
jg223fp <jg223fp@student.lnu.se>
2023-02-21 17:02:00 +0100

    Added FreeRTOS example code and reduced it

--- src/movement_monitoring/movement_monitoring.ino
+++ src/movement_monitoring/movement_monitoring.ino
@@ -1,201 +1,72 @@
+#define ARDUINO_RUNNING_CORE 1
 
-#include <FreeRTOS.h>
-#include <task.h>
-#include <Wire.h>
-#include <Adafruit_AMG88xx.h>
+// Define two tasks for Blink & AnalogRead.
+void TaskBlink( void *pvParameters );
+void TaskAnalogRead( void *pvParameters );
+TaskHandle_t analog_read_task_handle; // You can (don't have to) use this to be able to manipulate a task from somewhere else.
 
-// ##############  Global variables  ##############
-Adafruit_AMG88xx amg;
-float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
-int row1[8];  // Bottom of sensor (eye towards face and text is readable)
-int row2[8];   
-int row3[8];
-int row4[8];
-int row5[8];
-int row6[8];
-int row7[8];
-int row8[8]; // Top of sensor
-int old_row1_avg, old_row2_avg, old_row3_avg, old_row4_avg = 0;
-int old_row5_avg, old_row6_avg, old_row7_avg, old_row8_avg = 0;
-int average_temp;
-int left_to_right = 0;
-int right_to_left = 0;
-
-
-// ################  Tasks  ########################
-
-// Blinks the builtin led
-void blink(void *param) {
-  (void) param;
-  pinMode(LED_BUILTIN, OUTPUT);
-  while (true) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(750);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-  }
-}
-
-// Reads all the pixels on the AMG8833 and prints the values in the serial communication.
-void amg_read(void *param) {
-  (void) param;
-  
-  while(true) {
-    //read all the pixels
-    amg.readPixels(pixels);
-  /*
-    Serial.print("[");
-    for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
-      Serial.print(pixels[i-1]);
-      Serial.print(", ");
-      if( i%8 == 0 ) Serial.println();
-    }
-    Serial.println("]");
-    Serial.println();
-  */
-
-  for (int i = 0; i < 8; i++) {
-    int index = i * 8;
-    row1[i] = pixels[index];
-    index++;
-    row2[i] = pixels[index];
-    index++;
-    row3[i] = pixels[index];
-    index++;
-    row4[i] = pixels[index];
-    index++;
-    row5[i] = pixels[index];
-    index++;
-    row6[i] = pixels[index];
-    index++;
-    row7[i] = pixels[index];
-    index++;
-    row8[i] = pixels[index];
-  }
-
-  int row1_avg = get_row_average(row1);
-  int row2_avg = get_row_average(row2);
-  int row3_avg = get_row_average(row3);
-  int row4_avg = get_row_average(row4);
-  int row5_avg = get_row_average(row5);
-  int row6_avg = get_row_average(row6);
-  int row7_avg = get_row_average(row7);
-  int row8_avg = get_row_average(row8);
-
-  //if sasts from hell should be here somwhere
-  
-
-
-
-  if(row1_avg - old_row1_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row2_avg - old_row2_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row3_avg - old_row3_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row4_avg - old_row4_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row5_avg - old_row5_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row6_avg - old_row6_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row7_avg - old_row7_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  if(row8_avg - old_row8_avg > 1) {
-    Serial.print("O ");
-  } else {
-    Serial.print("  ");
-  }
-  Serial.println();
-  
-  delay(1);
-  }
-}
-
-// Calculate the average temp of all pixels of AMG8833 sensor every XX second
-void amg_avg_temp(void *param) {
-  (void) param;
-  while(true) {
-    old_row1_avg = get_row_average(row1);
-    old_row2_avg = get_row_average(row2);
-    old_row3_avg = get_row_average(row3);
-    old_row4_avg = get_row_average(row4);
-    old_row5_avg = get_row_average(row5);
-    old_row6_avg = get_row_average(row6);
-    old_row7_avg = get_row_average(row7);
-    old_row8_avg = get_row_average(row8);
-  delay(10000); // Set how often the average should be calculated here.
-  }
-}
-
-
-// ###################  Functions ############
-
-// Initiates communication with the AMG8833 via i2c.
-void amg_init() {
-    Serial.println("Initiating AMG8833");
-    bool status;
-    status = amg.begin();
-    if (!status) {
-        Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-        while (1);
-    }
-    delay(1000); // let sensor boot up
-    amg.readPixels(pixels);
-}
-
-// Calculate the varegare of an array with 8 values
-int get_row_average(int arr[8]) {
-  int sum = 0;
-    for (int i = 0; i < 8; i++) {     
-       sum = sum + arr[i];    
-  }
-  int avg = sum/8;
-  return avg;
-}
-
-
-// ########## Main programs ######################
-// Running on core0
+// The setup function runs once when you press reset or power on the board.
 void setup() {
+  // Initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
-  delay(100);
-  amg_init();
-  xTaskCreate(amg_avg_temp, "avgTempCalc", 256, nullptr, 5, nullptr);
-  delay(100);
-  xTaskCreate(blink, "BLINK", 128, nullptr, 6, nullptr);
-  xTaskCreate(amg_read, "TempRead", 1024, nullptr, 7, nullptr);
+  // Set up two tasks to run independently.
+  uint32_t blink_delay = 1000; // Delay between changing state on LED pin
+  xTaskCreate(
+    TaskBlink
+    ,  "Task Blink" // A name just for humans
+    ,  2048        // The stack size can be checked by calling `uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);`
+    ,  (void*) &blink_delay // Task parameter which can modify the task behavior. This must be passed as pointer to void.
+    ,  2  // Priority
+    ,  NULL // Task handle is not used here - simply pass NULL
+    );
+
+  // This variant of task creation can also specify on which core it will be run (only relevant for multi-core ESPs)
+  xTaskCreatePinnedToCore(
+    TaskAnalogRead
+    ,  "Analog Read"
+    ,  2048  // Stack size
+    ,  NULL  // When no parameter is used, simply pass NULL
+    ,  1  // Priority
+    ,  &analog_read_task_handle // With task handle we will be able to manipulate with this task.
+    ,  ARDUINO_RUNNING_CORE // Core on which the task will run
+    );
+
+  Serial.printf("Basic Multi Threading Arduino Example\n");
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void loop() {
+void loop(){
+
 }
 
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
 
-// Running on core1
-void setup1() {
-  delay(5000);
+void TaskBlink(void *pvParameters){  // This is a task.
+  uint32_t blink_delay = *((uint32_t*)pvParameters);
+
+  pinMode(25, OUTPUT);
+
+  while (true){ // A Task shall never return or exit.
+    digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+    // arduino-esp32 has FreeRTOS configured to have a tick-rate of 1000Hz and portTICK_PERIOD_MS
+    // refers to how many milliseconds the period between each ticks is, ie. 1ms.
+    Serial.println("blink");
+    delay(blink_delay);
+    digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
+    delay(blink_delay);
+  }
 }
 
-void loop1() {
+void TaskAnalogRead(void *pvParameters){ 
+  (void) pvParameters;
+
+  while (true){
+    // read the input on analog pin:
+    int sensorValue = 11;
+    // print out the value you read:
+    Serial.println(sensorValue);
+    delay(5000); // 100ms delay
+  }
 }
